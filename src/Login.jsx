718df9b49c8405f0264315a95c6f81db44b10095
BigNitgh <edu_armas11@hotmail.com>
2023-03-27 13:59:41 -0500

    corfi

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,7 +4,7 @@ export const Navbar = ()=>{
   return (
     <div>
       <h1>estoy en el Login</h1>
-      <button class ="boton"onClick={clickeame}>click me</button>
+      <button className = 'boton' onClick={clickeame}>click me</button>
     </div>
   )
 }
