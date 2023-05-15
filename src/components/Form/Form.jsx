b8fa3e8eeb956265688e5e50eaf83e858451513b
Edu Walter Armas Aliaga <edu_armas11@hotmail.com>
2023-05-15 15:43:07 -0500

    avances

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,28 +1,46 @@
 import { useState } from "react";
 
 const Form = () => {
-  const [Nombre, setNombre] = useState("");
-  const [Apellido, setApellido] = useState("");
-  console.log(Apellido)
+  const [UserData, setUserData] = useState({
+    Nombre: "",
+    Apellido: "",
+    Numero: ""
+  });
+
+  const Enviarformulario = (e) => {
+    e.preventDefault();
+  };
+
+  console.log(UserData);
 
   return (
     <div>
       <h1>Estoy en el formulario</h1>
 
-      <form action="">
-        <input type="text" name="nombre" placeholder="Ponga su Nombre" />
-
+      <form onSubmit={Enviarformulario} action="">
         <input
           type="text"
           name="nombre"
-          value={Apellido}
-          placeholder="Ponga su Apelli  do"
+          value={UserData.Nombre}
+          placeholder="Ponga su Nombre"
           onChange={(e) => {
-            setApellido(e.target.value);
+            setUserData({ ...UserData, Nombre: e.target.value });
           }}
         />
 
-        <input type="text" name="nombre" placeholder="Ponga su Numero" />
+        <input
+          type="text"
+          name="apellido"
+          value={UserData.Apellido}
+          placeholder="Ponga su Apellido"
+          onChange={(e) => {
+            setUserData({ ...UserData, Apellido: e.target.value });
+          }}
+        />
+
+        <input type="text" name="numero" value={UserData.Numero} onChange={(e)=>{
+          setUserData({ ...UserData, Numero: e.target.value})
+        }} placeholder="Ponga su Numero" />
         <input type="submit" />
       </form>
     </div>
